edd4458b1d952b7e22b87fa93ab9064346ca1768
Odysseas Trispiotis <oddo1994.ot@gmail.com>
2019-05-19 23:05:30 +0300

    Add Eureka config

--- src/main/java/gr/di/uoa/m151/M151ApiApplication.java
+++ src/main/java/gr/di/uoa/m151/M151ApiApplication.java
@@ -2,8 +2,10 @@ package gr.di.uoa.m151;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class M151ApiApplication {
 
     public static void main(String[] args) {
